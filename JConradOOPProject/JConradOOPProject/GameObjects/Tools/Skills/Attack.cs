c6aa132ddbfc3301dd605f2b14d941b48d4d9102
yordan-sp <yordan_sp@hotmail.com>
2014-02-22 22:06:55 +0200

    Attack.cs

---
+++ JConradOOPProject/JConradOOPProject/GameObjects/Tools/Skills/Attack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JConradOOPProject.GameObjects.Tools.Skills
+{
+    class Attack : Item
+    {
+        //fields
+        private byte levelOfSkill;
+        private bool alwaysHit;
+
+        //constructors
+        public Attack(string inputName, decimal inputPrice)
+            : base(inputName, inputPrice)
+        { }
+
+        public Attack(string inputName, decimal inputPrice, byte inputLevel)
+            : base(inputName, inputPrice)
+        {
+            this.levelOfSkill = inputLevel;
+        }
+
+        //properties
+        public bool AlwaysHit
+        {
+            get
+            {
+                return this.alwaysHit;
+            }
+            set 
+            {
+                this.alwaysHit = true;
+            }
+        }
+
+        //methods
+
+
+    }
+}
