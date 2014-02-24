090df2d89a6cc3b1675b6ba782ab43e584218ca7
yordan-sp <yordan_sp@hotmail.com>
2014-02-24 19:34:53 +0200

    List of Tools removed.

--- JConradOOPProject/JConradOOPProject/GameObjects/Tools/Shields/Shield.cs
+++ JConradOOPProject/JConradOOPProject/GameObjects/Tools/Shields/Shield.cs
@@ -1,12 +1,5 @@
 ﻿namespace JConradOOPProject.GameObjects.Tools.Shields
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using JConradOOPProject.GameObjects.Tools.Weapons;
-
     public abstract class Shield : Item
     {
         public Shield()
