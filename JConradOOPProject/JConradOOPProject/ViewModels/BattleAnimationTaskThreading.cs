eef28bc30adf536d2471e9e208a44005ad12f84e
hAWKdv <hawk94@abv.bg>
2014-02-27 18:41:01 +0200

    Added health decreasing system
    - Added health decreasing system (animation; under development)
    - Separated GUI manipulation code execution (animations) from main
    thread (BattleAnimationTaskThreading.cs)

---
+++ JConradOOPProject/JConradOOPProject/ViewModels/BattleAnimationTaskThreading.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JConradOOPProject.ViewModels
+{
+    /// <summary>
+    /// BattleAnimationTaskThreading is used for separating the tasks of the GUI and the Worker via multithreading.
+    /// </summary>
+    public static class BattleAnimationTaskThreading
+    {
+        public static int currentWidth = 0;
+        public static int decreaseWidth = 0;
+
+        /// <summary>
+        /// Passes the animation operation of health decreasing to a new thread.
+        /// </summary>
+        /// <param name="progress"></param>
+        public static void AdjustHealth(IProgress<int> progress)
+        {
+            //if (decreaseWidth < 0)
+            //{
+            //    throw new ArgumentOutOfRangeException("The decreasing value must not exceed the total health.");
+            //}
+
+            for (int pixels = currentWidth; pixels >= decreaseWidth; pixels--)
+            {
+                Task.Delay(15).Wait();
+                progress.Report(pixels);
+            }
+        }
+    }
+}
--- JConradOOPProject/JConradOOPProject/Views/Battle.xaml.cs
+++ JConradOOPProject/JConradOOPProject/Views/Battle.xaml.cs
@@ -15,12 +15,21 @@ using System.Windows.Shapes;
 
 namespace JConradOOPProject.Views
 {
+    using JConradOOPProject.ViewModels;
+
     /// <summary>
     /// Interaction logic for Battle.xaml
     /// </summary>
     public partial class Battle : UserControl
     {
+        const int INITIAL_HEALTH = 100;
+        const int HEALTH_BAR_MAXWIDTH = 264; // Pixels
+
         private string location; // The field is not accessed directly nor can be modified by the user
+
+        // To be synced with GameEngine
+        private int playerHealth;
+        private int enemyHealth;
 
         public Battle(string inputLocation)
         {
@@ -30,8 +39,34 @@ namespace JConradOOPProject.Views
             this.SetLocation();
 
             // Test purposes
-            this.SetHealthBarPts(this.PlayerHealthBar, 45);
-            this.SetHealthBarPts(this.EnemyHealthBar, 82);
+            this.PlayerHealth = 45;
+            this.EnemyHealth = 82;
+        }
+
+        /// <summary>
+        /// Player's health property used for initial set. Performs an automatic change of the health bar aswell.
+        /// </summary>
+        public int PlayerHealth
+        {
+            get { return this.playerHealth; }
+            set
+            {
+                this.playerHealth = value;
+                this.SetHealthBarPts(this.PlayerHealthBar, this.playerHealth);
+            }
+        }
+
+        /// <summary>
+        /// Enemy's health property used for initial set. Performs an automatic change of the health bar aswell.
+        /// </summary>
+        public int EnemyHealth
+        {
+            get { return this.enemyHealth; }
+            set
+            {
+                this.enemyHealth = value;
+                this.SetHealthBarPts(this.EnemyHealthBar, this.enemyHealth);
+            }
         }
 
         /// <summary>
@@ -48,33 +83,64 @@ namespace JConradOOPProject.Views
         /// </summary>
         /// <param name="target"></param>
         /// <param name="points"></param>
-        private void SetHealthBarPts(object target, byte points)
+        private void SetHealthBarPts(object target, int points)
         {
             Border bar = (Border)target;
-            int barWidth = 264 * points / 100; // 264 - Bar Max Width
+            int barWidth = this.HealthToPixels(points);
 
             bar.DataContext = new { Health = points, HealthPerc = barWidth };
         }
 
         /// <summary>
-        /// Provides a smooth animation of health decreasing
+        /// Converts health value to the respective pixel value of the health bar
         /// </summary>
-        /// <param name="target"></param>
-        /// <param name="decreaseWith"></param>
-        private void DecreaseHealth(object target, byte decreaseWith)
+        /// <param name="points"></param>
+        /// <returns></returns>
+        private int HealthToPixels(int points)
         {
-            byte currentHealth = 45; // HARDCODED - TO BE CHANGED
+            return HEALTH_BAR_MAXWIDTH * points / INITIAL_HEALTH;
+        }
 
-            if (decreaseWith > currentHealth)
+        /// <summary>
+        /// Decreases target's health (with an animation)
+        /// </summary>
+        /// <param name="getter"></param>
+        /// <param name="setter"></param>
+        /// <param name="decreaseValue"></param>
+        private async void DecreaseHealth(Func<int> getter, Action<int> setter, Border target, int decreaseValue)
+        {
+            if (decreaseValue > getter())
             {
-                throw new ArgumentOutOfRangeException("The decreasing value must not exceed the total health.");
+                // Problems with catching exception from async method.
+                MessageBox.Show("Decr value is bigger than actual value.", "Fatal Error");
+                return;
             }
 
-            for (byte health = currentHealth; health >= currentHealth - decreaseWith; health--)
-            {
-                System.Threading.Thread.Sleep(150);
-                this.SetHealthBarPts(target, health);
-            }
+            int currentHealthPxl = this.HealthToPixels(getter());
+
+            setter(getter() - decreaseValue);
+            
+            int decreasedHealthPxl = this.HealthToPixels(getter());
+
+            //System.Threading.Thread.Sleep(3000);
+
+            // Transfers the data to the static class (cant be passed as method arguments)
+            BattleAnimationTaskThreading.currentWidth = currentHealthPxl;
+            BattleAnimationTaskThreading.decreaseWidth = decreasedHealthPxl;
+
+            var progress = new Progress<int>(ct => target.DataContext = new { Health = getter(), HealthPerc = ct });
+            await Task.Factory.StartNew(() => BattleAnimationTaskThreading.AdjustHealth(progress), TaskCreationOptions.LongRunning);
+        }
+
+        /// <summary>
+        /// Test purposes
+        /// </summary>
+        private void ButtonAnim_Click(object sender, RoutedEventArgs e)
+        {
+            this.DecreaseHealth(() => this.playerHealth, n => this.playerHealth= n, this.PlayerHealthBar, 22);
+
+            // One animation at time.
+            //this.DecreaseHealth(() => this.enemyHealth, n => this.enemyHealth = n, this.EnemyHealthBar, 70);
         }
     }
 }
